74c0e49ae0fcc6104f0b11211ecb06de90618085
mliev <service@mliev.com>
2025-09-08 16:38:47 +0800

    feat(config): add installed assembly and adjust assembly order
    Add installed assembly to handle installation detection, and optimize the order of assembly initialization.

--- config/assembly.go
+++ config/assembly.go
@@ -5,6 +5,7 @@ import (
 	configAssembly "cnb.cool/mliev/open/dwz-server/internal/pkg/config/assembly"
 	databaseAssembly "cnb.cool/mliev/open/dwz-server/internal/pkg/database/assembly"
 	envAssembly "cnb.cool/mliev/open/dwz-server/internal/pkg/env/assembly"
+	installedAssembly "cnb.cool/mliev/open/dwz-server/internal/pkg/installed/assembly"
 	loggerAssembly "cnb.cool/mliev/open/dwz-server/internal/pkg/logger/assembly"
 	redisAssembly "cnb.cool/mliev/open/dwz-server/internal/pkg/redis/assembly"
 )
@@ -17,13 +18,11 @@ type Assembly struct {
 func (receiver *Assembly) Get() []interfaces.AssemblyInterface {
 
 	return []interfaces.AssemblyInterface{
-		&envAssembly.Env{Helper: receiver.Helper}, // 环境变量
-		&configAssembly.Config{ // 代码中的配置(可使用环境变量)
-			Helper:         receiver.Helper, // 注入
-			DefaultConfigs: Config{}.Get(),  //注入默认配置
-		},
-		&loggerAssembly.Logger{Helper: receiver.Helper},     // 日志驱动
-		&databaseAssembly.Database{Helper: receiver.Helper}, // 数据库配置
-		&redisAssembly.Redis{Helper: receiver.Helper},       // redis 配置
+		&envAssembly.Env{Helper: receiver.Helper},                                       // 环境变量
+		&configAssembly.Config{Helper: receiver.Helper, DefaultConfigs: Config{}.Get()}, // 代码中的配置(可使用环境变量)
+		&loggerAssembly.Logger{Helper: receiver.Helper},                                 // 日志驱动
+		&installedAssembly.Installed{Helper: receiver.Helper},                           // 安装检测
+		&databaseAssembly.Database{Helper: receiver.Helper},                             // 数据库配置
+		&redisAssembly.Redis{Helper: receiver.Helper},                                   // redis 配置
 	}
 }
