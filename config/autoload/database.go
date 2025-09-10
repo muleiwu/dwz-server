5d8995d2a2e3035a913c86913f03d6e99f0f88e0
mliev <service@mliev.com>
2025-09-10 23:36:21 +0800

    fix(config): adjust database.go import path and localhost usage

--- config/autoload/database.go
+++ config/autoload/database.go
@@ -1,6 +1,8 @@
 package autoload
 
-import envInterface "cnb.cool/mliev/open/dwz-server/internal/interfaces"
+import (
+	envInterface "cnb.cool/mliev/open/dwz-server/internal/interfaces"
+)
 
 type Database struct {
 }
@@ -8,7 +10,7 @@ type Database struct {
 func (receiver Database) InitConfig(helper envInterface.HelperInterface) map[string]any {
 	return map[string]any{
 		"database.driver":   helper.GetEnv().GetString("database.driver", "mysql"),
-		"database.host":     helper.GetEnv().GetString("database.host", "127.0.0.1"),
+		"database.host":     helper.GetEnv().GetString("database.host", "localhost"),
 		"database.port":     helper.GetEnv().GetInt("database.port", 3306),
 		"database.dbname":   helper.GetEnv().GetString("database.dbname", "dwz"),
 		"database.username": helper.GetEnv().GetString("database.username", "dwz"),
