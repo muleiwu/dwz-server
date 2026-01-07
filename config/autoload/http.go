f1ba48c00c182e13e6b6bf2ec01f68088213bb08
mliev <service@mliev.com>
2026-01-07 09:24:17 +0800

    fix(config): 修正HTTP模板模式环境变量名

--- config/autoload/http.go
+++ config/autoload/http.go
@@ -12,7 +12,7 @@ func (receiver Http) InitConfig(helper envInterface.HelperInterface) map[string]
 		"http.load_static":    helper.GetEnv().GetBool("http.load_static", true),
 		"http.static_mode":    helper.GetEnv().GetString("http.static_mode", "embed"),       // embed 或 disk
 		"http.static_dir":     helper.GetEnv().GetString("http.static_dir", "static"),       // 静态资源根目录
-		"http.templates_mode": helper.GetEnv().GetString("http.static_dir", "embed"),        // embed 或 disk
+		"http.templates_mode": helper.GetEnv().GetString("http.templates_mode", "embed"),    // embed 或 disk
 		"http.templates_dir":  helper.GetEnv().GetString("http.templates_dir", "templates"), // 模板目录
 	}
 }
